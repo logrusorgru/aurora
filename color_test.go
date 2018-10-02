a7b3b318ed4e1ae5b80602b08627267303c68572
Konstantin Ivanov <kostyarin.ivanov@gmail.com>
2018-10-02 22:45:14 +0300

    fix name typo

--- color_test.go
+++ color_test.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2016 Konstanin Ivanov <kostyarin.ivanov@gmail.com>.
+// Copyright (c) 2016 Konstantin Ivanov <kostyarin.ivanov@gmail.com>.
 // All rights reserved. This program is free software. It comes without
 // any warranty, to the extent permitted by applicable law. You can
 // redistribute it and/or modify it under the terms of the Do What
